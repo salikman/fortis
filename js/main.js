c5f93c05607cadce1f8ead4fbb51883b2c643819
Max Savonik <savonikmaksim@gmail.com>
2020-07-09 11:02:14 +0300

    v1.7

--- js/main.js
+++ js/main.js
@@ -12,9 +12,7 @@
 		var wScroll = $(this).scrollTop();
 
 		// Fixed nav
-		// if ()
 		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
-
 	});
 
 	// slider
@@ -27,51 +25,5 @@
 		dotsClass: 'intro-dots',
 		pauseOnHover: false,
 		pauseOnFocus: false,
-	});
-	$('#partners-slider').slick({
-		infinite: true,
-		slidesToShow: 6,
-		slidesToScroll: 1,
-		speed: 800,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		arrows: false,
-		responsive: [
-			{
-				breakpoint: 1367,
-				settings: {
-					slidesToShow: 5,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 1025,
-				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 769,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 481,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 381,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
 	});
 })(jQuery);
